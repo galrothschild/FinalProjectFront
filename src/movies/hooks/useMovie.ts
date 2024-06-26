0f61a268586dc448bc17edd86329e1bda27711fb
rothschild <gal.rothschildf+git@gmail.com>
2024-06-26 19:13:23 +0300

    cleanup

--- src/movies/hooks/useMovie.ts
+++ src/movies/hooks/useMovie.ts
@@ -5,7 +5,6 @@ import type { IMovie } from "../models/IMovie.model";
 export const useMovie = (id: number) => {
 	const [movie, setMovie] = useState<IMovie>();
 	const [loading, setLoading] = useState(true);
-	// biome-ignore lint/correctness/useExhaustiveDependencies: needs to run once.
 	useEffect(() => {
 		const fetchMovie = async () => {
 			const response = await getFromApi(`/movies/${id}`);
--- src/movies/pages/MoviePage.tsx
+++ src/movies/pages/MoviePage.tsx
@@ -1,7 +1,5 @@
-import React from "react";
 import { useParams } from "react-router-dom";
 import { useMovie } from "../hooks/useMovie";
-import { Avatar, Card } from "flowbite-react";
 
 const MoviePage = () => {
 	const { id } = useParams<{ id: string }>();
